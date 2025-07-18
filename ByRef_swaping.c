fc8a2ff5c0bd0034c1c13c9473b5fa0c9dedb2e5
Parth <parthpatel5869@icloud.com>
2025-07-18 10:13:56 +0530

    Data Structure

--- ByRef_swaping.c
+++ ByRef_swaping.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+void swap(int *, int *);
+
 int main()
 {
     int a, b;
@@ -9,8 +11,18 @@ int main()
 
     printf("Enter No b: ");
     scanf("%d", &b);
+    printf("After Swaping tha value a: %d & b: %d \n", a,b);
 
-    
+    swap(&a, &b);
 
     return 0;
+}
+
+void swap(int *a, int *b)
+{   
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
+    printf("Befor Swaping tha value a: %d & b: %d \n", a,b);
 }
